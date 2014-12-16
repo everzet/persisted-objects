341f02baa5a52776417dcac67844ca5ee0ae864f
everzet <ever.zet@gmail.com>
2014-12-16 19:05:29 +0000

    Implement FileRepository

---
+++ src/FileRepository.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Everzet\PersistedObjects;
+
+use ReflectionMethod;
+
+final class FileRepository implements Repository
+{
+    private $filename;
+    private $identityAccessor;
+
+    public function __construct($filename, ReflectionMethod $identityAccessor)
+    {
+        $this->filename = $filename;
+        $this->identityAccessor = $identityAccessor;
+    }
+
+    public function save($object)
+    {
+        $id = $this->stringify($this->objectId($object));
+        $db = $this->loadDb();
+        $db[$id] = $object;
+        $this->saveDb($db);
+    }
+
+    public function remove($object)
+    {
+        $id = $this->stringify($this->objectId($object));
+        $db = $this->loadDb();
+        unset($db[$id]);
+        $this->saveDb($db);
+    }
+
+    public function findById($id)
+    {
+        $id = $this->stringify($id);
+        $db = $this->loadDb();
+
+        if (!isset($db[$id])) {
+            return null;
+        }
+
+        return $db[$id];
+    }
+
+    public function getAll()
+    {
+        return array_values($this->loadDb());
+    }
+
+    public function clear()
+    {
+        $this->saveDb([]);
+    }
+
+    private function objectId($object)
+    {
+        return $this->identityAccessor->invoke($object);
+    }
+
+    private function stringify($object)
+    {
+        return md5(var_export($object, true));
+    }
+
+    private function loadDb()
+    {
+        return file_exists($this->filename)
+            ? unserialize(file_get_contents($this->filename))
+            : [];
+    }
+
+    private function saveDb(array $collection)
+    {
+        file_put_contents($this->filename, serialize($collection));
+    }
+}
---
+++ tests/FileRepositoryTest.php
@@ -0,0 +1,144 @@
+<?php
+
+class FileRepositoryTest extends PHPUnit_Framework_TestCase
+{
+    private $filename;
+
+    protected function setUp()
+    {
+        $this->filename = sys_get_temp_dir() . '/test_repository';
+    }
+
+    protected function tearDown()
+    {
+        file_exists($this->filename) && unlink($this->filename);
+    }
+
+    /** @test */ function shouldBeAbleToSaveAndRetrieveObjectLater()
+    {
+        $objectToPersist = new PersistedObject($objectId = 42, 'everzet');
+        $repository = $this->createRepository();
+
+        $repository->save($objectToPersist);
+
+        $this->assertEquals($objectToPersist, $repository->findById($objectId));
+    }
+
+    /** @test */ function shouldBeAbleToSaveAndRetrieveObjectsWithIDAsVO()
+    {
+        $objectToPersist = new PersistedObject($objectId = (object)42, 'everzet');
+        $repository = $this->createRepository();
+
+        $repository->save($objectToPersist);
+
+        $this->assertEquals($objectToPersist, $repository->findById($objectId));
+    }
+
+    /** @test */ function shouldOverrideObjectsWithTheSameId()
+    {
+        $objectsToPersist = [
+            new PersistedObject($objectId = 42, 'everzet'),
+            new PersistedObject($objectId, 'marcello')
+        ];
+        $repository = $this->createRepository();
+
+        $repository->save($objectsToPersist[0]);
+        $repository->save($objectsToPersist[1]);
+
+        $this->assertEquals($objectsToPersist[1], $repository->findById($objectId));
+    }
+
+    /** @test */ function shouldBeAbleToRemoveObject()
+    {
+        $objectToPersist = new PersistedObject($objectId = 42, 'everzet');
+        $repository = $this->createRepository();
+
+        $repository->save($objectToPersist);
+        $repository->remove($objectToPersist);
+
+        $this->assertNull($repository->findById($objectId));
+    }
+
+    /** @test */ function shouldDoNothingWhenAskedToRemoveUnstoredObject()
+    {
+        $objectToPersist = new PersistedObject($objectId = 42, 'everzet');
+        $repository = $this->createRepository();
+
+        $repository->remove($objectToPersist);
+
+        $this->assertEquals([], $repository->getAll());
+    }
+
+    /** @test */ function shouldBeAbleToGetAllObjects()
+    {
+        $objectsToPersist = [
+            new PersistedObject(42, 'everzet'),
+            new PersistedObject(24, 'marcello')
+        ];
+        $repository = $this->createRepository();
+
+        $repository->save($objectsToPersist[0]);
+        $repository->save($objectsToPersist[1]);
+
+        $newRepository = $this->createRepository();
+        $this->assertEquals($objectsToPersist, $repository->getAll());
+        $this->assertEquals($repository->getAll(), $newRepository->getAll());
+    }
+
+    /** @test */ function shouldPersistObjectsBetweenInstances()
+    {
+        $objectsToPersist = [
+            new PersistedObject(42, 'everzet'),
+            new PersistedObject(24, 'marcello')
+        ];
+        $repository = $this->createRepository();
+        $newRepository = $this->createRepository();
+
+        $repository->save($objectsToPersist[0]);
+        $repository->save($objectsToPersist[1]);
+
+        $this->assertEquals($repository->getAll(), $newRepository->getAll());
+    }
+
+    /** @test */ function shouldBeAbleToClearRepository()
+    {
+        $objectsToPersist = [
+            new PersistedObject(42, 'everzet'),
+            new PersistedObject(24, 'marcello')
+        ];
+        $repository = $this->createRepository();
+        $newRepository = $this->createRepository();
+
+        $repository->save($objectsToPersist[0]);
+        $repository->save($objectsToPersist[1]);
+        $repository->clear();
+
+        $this->assertEquals([], $repository->getAll());
+        $this->assertEquals($newRepository->getAll(), $repository->getAll());
+    }
+
+    private function createRepository()
+    {
+        return new Everzet\PersistedObjects\FileRepository(
+            $this->filename,
+            new ReflectionMethod(PersistedObject::class, 'getId')
+        );
+    }
+}
+
+class PersistedObject
+{
+    private $id;
+    private $name;
+
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+}
